c4706343ecd9d01e1df725bf02634669bcf60be5
Merijn <prim@pr1mula.nl>
2023-01-30 15:27:55 +0100

    changes

--- src/elements/Dock/DockContent.jsx
+++ src/elements/Dock/DockContent.jsx
@@ -4,7 +4,7 @@ import "@styles/dock.scss";
 import FinderIcon from "@static/finder.png";
 import CodeIcon from "@static/code.png";
 import GithubIcon from "@static/github.png";
-import twitterIcon from "@static/twitterIcon.png";
+import instagramIcon from "@static/instagramIcon.png";
 import FolderIcon from "@static/folder.png";
 import LinkedInIcon from "@static/LinkedInIcon.png";
 import NothingIcon from "@static/NothingIcon.png";
@@ -52,10 +52,10 @@ const DockContent = () => {
 									/>
 								</li>
 							</Link>
-							<a href="">
-								<li data-title="Twitter">
+							<a href="https://www.instagram.com/boba_the_rescue/">
+								<li data-title="My Dog's instagram">
 									<img
-										src={twitterIcon}
+										src={instagramIcon}
 										className="img-fluid"
 										alt="mac"
 									/>
